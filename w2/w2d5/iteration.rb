007a836e198387a9eddd944b2fd8d91b91dcafd3
apopma <adam.popma@gmail.com>
2015-06-06 03:13:11 +0000

    Monkey-patch method classes

--- w2/w2d5/iteration.rb
+++ w2/w2d5/iteration.rb
@@ -1,9 +1,11 @@
-def factors(num)
-  factors = []
-  1.upto(num) do |x|
-    factors << x if num % x == 0
+class Fixnum
+  def factors
+    factors = []
+    1.upto(self) do |x|
+      factors << x if self % x == 0
+    end
+    factors
   end
-  factors
 end
 
 # -----------------------------------------------------------------------------
@@ -29,46 +31,50 @@ end
 
 # -----------------------------------------------------------------------------
 
-def substrings(string)
-  # could probably be done without whiles and iterators,
-  # but I don't know how to write a cleaner way to loop like this
-  substrings = []
-  outer_char = 0
-  inner_char = 0
+class String
   
-  while outer_char < string.length
-    while inner_char < string.length
-      substrings << string[outer_char..inner_char]
-      inner_char += 1
+  def substrings
+    # could probably be done without whiles and iterators,
+    # but I don't know how to write a cleaner way to loop like this
+    substrings = []
+    outer_char = 0
+    inner_char = 0
+    
+    while outer_char < self.length
+      while inner_char < self.length
+        substrings << self[outer_char..inner_char]
+        inner_char += 1
+      end
+      
+      outer_char += 1
+      inner_char = outer_char
+    end
+    substrings
+  end
+
+
+  def subwords
+    all_substrings = self.substrings
+    words = []
+    
+    File.foreach("dictionary.txt") do |line|
+      words << line.chomp if all_substrings.include?(line.chomp)
     end
     
-  outer_char += 1
-  inner_char = outer_char
-  end
-  substrings
-end
-
-
-def subwords(string)
-  all_substrings = substrings(string)
-  words = []
-  
-  File.foreach("dictionary.txt") do |line|
-    words << line.chomp if all_substrings.include?(line.chomp)
+    words
   end
   
-  words
 end
 
 # =============================================================================
 
-p factors(418)
+p 418.factors
 
 ary_to_sort = [9, 7, 1, 4, 8, 0, 2, 3, 5, 6]
 p ary_to_sort
 ary_to_sort.bubble_sort!
 p ary_to_sort
 
-p substrings("cat")
-p subwords("supercalifragilisticexpialidocious")
-p subwords("antidisestablishmentarianism")
+p "cat".substrings
+p "supercalifragilisticexpialidocious".subwords
+p "antidisestablishmentarianism".subwords
