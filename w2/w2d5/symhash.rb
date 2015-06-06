df50b06d0860707712699aca0f3323d9a7897230
apopma <adam.popma@gmail.com>
2015-06-06 03:03:36 +0000

    Finish symhash.rb

--- w2/w2d5/symhash.rb
+++ w2/w2d5/symhash.rb
@@ -1,3 +1,14 @@
-def super_print(string)
+def super_print(string, args = {})
+    defaults = { times: 1,
+                 upcase: false,
+                 reverse: false }
+    options = defaults.merge(args)
     
+    string.upcase! if options[:upcase]
+    string.reverse! if options[:reverse]
+    
+    options[:times].times {print string + ' '}
+    puts "\n"   # is there a nicer-looking way to insert this \n?
 end
+
+super_print("goblin", times: 3, upcase: true, reverse: true)
