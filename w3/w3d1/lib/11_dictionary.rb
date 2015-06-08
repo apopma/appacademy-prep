db635ee295be36add4522f44dde94bc370f3b482
apopma <adam.popma@gmail.com>
2015-06-08 17:51:00 +0000

    improve Dictionary#sort

--- w3/w3d1/lib/11_dictionary.rb
+++ w3/w3d1/lib/11_dictionary.rb
@@ -1,6 +1,3 @@
-require 'byebug'
-
-
 class Dictionary
   attr_accessor :dict
   
@@ -16,34 +13,14 @@ class Dictionary
     @dict.keys.sort
   end
   
-  def find(key)
-    key_regex = /^((#{key})\w*)/
-    found = {}
-    @dict.each do |entry, description|
-      if entry =~ key_regex && !@dict[entry].nil?
-        found[entry] = description 
-      end 
-    end
-    found
-  end
-  
   def include?(keyword)
     @dict.keys.include?(keyword)
   end
   
+  def delete(entry)
+    @dict.delete(entry)
+  end
   
-  def sort
-    # Returns a new Hash; @dict sorted alphabetically by keys
-    # Replaces nil in entries without a description with ''
-    # Does not mutate @dict
-    sorted_entries = self.keywords
-    unsorted_descriptions = @dict.values
-    
-    unsorted_descriptions.map! { |desc| desc ||= '' }
-    sorted_descriptions = sorted_entries.map { |entry| @dict[entry] }
-    
-    Hash[sorted_entries.zip sorted_descriptions]
-  end  
   
   def add(entry)
     if entry.is_a? String
@@ -51,15 +28,31 @@ class Dictionary
     elsif entry.is_a? Hash
       entry.each_pair { |key, val| @dict[key] = val }
     else
-      raise TypeError, "A booming voice says, 'Wrong, cretin!', 
-            and you notice that you have turned into a pile of dust.
-            How inconvenient."
+      raise TypeError, "Dictionary#add expects String or Hash entries"
     end
   end
   
-  def delete(entry)
-    @dict.delete(entry)
+  
+  def find(key)
+    key_regex = /^((#{key})\w*)/
+    found = {}
+    @dict.each do |entry, description|
+      found[entry] = description if entry =~ key_regex && @dict[entry]
+    end
+    found
   end
+  
+  
+  def sort
+    # Returns a new Hash; @dict sorted alphabetically by keys
+    # Replaces nil in entries without a description with ''
+    # Does not mutate @dict
+    
+    sorted_entries = self.keywords
+    sorted_descriptions = sorted_entries.map { |entry| @dict[entry] }
+    Hash[sorted_entries.zip sorted_descriptions]
+  end  
+  
   
   def printable
     sorted = self.sort
@@ -67,5 +60,4 @@ class Dictionary
     sorted.each { |entry, desc| output << "[#{entry}] \"#{desc}\"\n" }
     output.chomp
   end
-  
 end
