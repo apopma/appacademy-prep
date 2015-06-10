86573a61e40d32d2de368f98b898eb01e055112f
apopma <adam.popma@gmail.com>
2015-06-10 00:38:39 +0000

    initial commit

---
+++ w3/w3d2/mastermind.rb
@@ -0,0 +1,22 @@
+class Code
+  def initialize
+  end
+  
+end
+
+# -----------------------------------------------------------------------------
+
+class Game
+  def initialize
+  end
+  
+end
+
+# =============================================================================
+
+def invoke
+end
+
+
+
+invoke if __FILE__ == $PROGRAM_NAME
