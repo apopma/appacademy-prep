4cf51c44586f224bf32d246593e3dc68c1c5673d
apopma <adam.popma@gmail.com>
2015-06-12 01:45:54 +0000

    phase I finished

--- w3/w3d4/lib/hangman.rb
+++ w3/w3d4/lib/hangman.rb
@@ -1,12 +1,13 @@
 class Hangman
   attr_reader :dictionary, :secret
   attr_accessor :partial, :guess
+  INPUT_REGEX = /^[a-z]$/
   
   def initialize
     @dictionary = File.readlines("lib/dictionary.txt")
     @secret = get_random_word
     @partial = "_" * secret.length
-    @guess = nil
+    @guess = ''
   end
   
   def play
@@ -14,12 +15,25 @@ class Hangman
     
     until won?
       puts "Secret word: #{partial}"
-      print "> "
-      guess = gets.chomp
+      
+      until guess =~ INPUT_REGEX
+        print "> "
+        guess = gets.chomp.downcase
+        puts "Sorry, I didn't understand that." unless guess =~ INPUT_REGEX
+        break if guess =~ INPUT_REGEX
+      end
+      
       secret.split('').map.with_index do |letter, idx|
         partial[idx] = letter if letter == guess
       end
     end
+    
+    puts "Winner!"
+      
+  end
+  
+  def valid_input?(char)
+    char.length == 1 && (a..z).to_a.include?(char)
   end
   
   def won?
