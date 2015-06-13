929a5d17be5ef81ed680a273dbbd2f9685aff151
apopma <adam.popma@gmail.com>
2015-06-13 14:43:21 +0000

    pathfinding partially works

--- w3/w3d5/maze_solver.rb
+++ w3/w3d5/maze_solver.rb
@@ -53,8 +53,10 @@ class MazeSolver
     choose_next_cell
     display
     
-    pathfind until closed_list.include?(finish)
-    puts "solve is done pathfinding! omg"
+    until closed_list.include?(finish)
+      pathfind
+      display
+    end
   end
   
   
@@ -129,29 +131,64 @@ class MazeSolver
   
   def pathfind
     open_list.delete(location)
-    closed_list << location
+    closed_list << location unless closed_list.include?(location)
     
     reachable_cells.each do |reachable_cell|
-      if !(open_list.include?(reachable_cell))
+      if open_list.include?(reachable_cell)
+        puts "#{reachable_cell} was already reachable"
+        update_movement_cost(reachable_cell)
+        
+      elsif !(open_list.include?(reachable_cell))
+        puts "#{reachable_cell} is newly reachable"
         open_list << reachable_cell 
         parent[reachable_cell] = location
-        
-      elsif open_list.include?(reachable_cell)
-        update_path_costs(reachable_cell)
-        puts "this space for rent"
       end
       
     end
-  
     
+    choose_next_cell
+    display
+    p "open list: #{open_list}"
+    p "closed list: #{closed_list}"
     
     puts "i'm pathfinding!"
-    closed_list << finish
   end
   
-  def update_path_costs(cell)
-    #only takes one cell, and doesn't immediately update movement_cost
+  
+  def update_movement_cost(cell)
+    if ortho?(cell, parent[cell])
+      movement_cost[cell] = movement_cost[parent[cell]] + MOVE_COST_ORTHO
+      
+    elsif diagonal?(cell, parent[cell])
+      movement_cost[cell] = movement_cost[parent[cell]] + MOVE_COST_DIAG
+    end
     
+    current_movement_cost = movement_cost[cell]
+    parent_movement_cost = movement_cost[parent[cell]]
+    puts "Updating movement costs for #{cell} with current parent #{parent[cell]}"
+    new_movement_cost = 0
+    
+    if ortho?(parent[cell], cell)
+      new_movement_cost = parent_movement_cost + MOVE_COST_ORTHO
+      
+    elsif diagonal?(parent[cell], cell)
+      new_movement_cost = parent_movement_cost + MOVE_COST_DIAG
+    end
+    
+    puts "current cost #{movement_cost[cell]}, 
+          parent cost #{movement_cost[parent[cell]]},
+          possible new cost #{new_movement_cost}"
+    
+    if new_movement_cost < current_movement_cost
+      puts "Found faster move #{new_movement_cost} for cell #{cell}"
+      puts "Previous parent of #{cell} was #{parent[cell]}"
+      movement_cost[cell] = new_movement_cost
+      parent[cell] = location
+      puts "Updated #{cell} parent to #{location}"
+      puts "Calculating new Manhattan and F scores..."
+      estimate_heuristic_costs(cell)
+      calculate_net_costs(cell)
+    end
   end
     
   
@@ -161,9 +198,16 @@ class MazeSolver
     estimate_heuristic_costs(open_list)
     calculate_net_costs(open_list)
     
-    puts "the next cell is #{net_movement_cost.key(net_movement_cost.values.min)}"
-    @location = net_movement_cost.key(net_movement_cost.values.min)
-    self[*location] = "•"
+    puts "movement costs: #{net_movement_cost.sort}"
+    
+    net_movement_cost.each do |pos, val|
+      if pos != @location && val == net_movement_cost.values.min && !closed_list.include?(pos)
+        @location = net_movement_cost.key(val)
+        puts "the next location is #{net_movement_cost.key(val)}"
+        self[*location] = "•"
+        return
+      end
+    end
   end
   
   def calculate_movement_costs(cells)
