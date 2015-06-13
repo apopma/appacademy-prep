435a3bd5b366b4daf4b51e6b808b168cc359a597
apopma <adam.popma@gmail.com>
2015-06-13 02:47:27 +0000

    first pathfinding step works

--- w3/w3d5/maze_solver.rb
+++ w3/w3d5/maze_solver.rb
@@ -1,10 +1,19 @@
 class MazeSolver
-  attr_reader :maze, :start, :finish
+  attr_reader :maze, :start, :finish, :location,
+              :open_list, :closed_list, :parents
+  MOVE_ORTHO = 10
+  MOVE_DIAG  = 14
   
   def initialize
     @maze   = parse(File.readlines(ARGV[0]))
     @start  = find_position('S')
     @finish = find_position('E')
+    
+    @location = start
+    @open_list = [start]
+    @closed_list = []
+    
+    @parents = {} #pos => parent's pos
   end
   
   def [](row, col)
@@ -19,6 +28,13 @@ class MazeSolver
     maze.each {|line| puts line.join}
     puts "start location is #{start}"
     puts "ending location is #{finish}"
+    puts "current location is #{location}"
+  end
+  
+  def listing
+    p "open cells: #{open_list}"
+    p "closed cells: #{closed_list}"
+    p "parents: #{parents}"
   end
   
   def parse(raw_maze)
@@ -40,6 +56,43 @@ class MazeSolver
     [maze.index(row), row.index(char)]
   end
   
+  
+  def reachable_cells
+    # Returns an array of all reachable cells from @location,
+    # ignoring walls, and cells on the closed list.
+    
+    curr_row = location[0]
+    curr_col = location[1]
+    
+    all_possible_moves = [ 
+          [curr_row, curr_col + 1],     [curr_row, curr_col - 1],
+          [curr_row + 1, curr_col],     [curr_row - 1, curr_col],
+          [curr_row + 1, curr_col + 1], [curr_row + 1, curr_col - 1],
+          [curr_row - 1, curr_col + 1], [curr_row - 1, curr_col - 1]
+                         ]
+                         
+    all_possible_moves.reject do |this_move|
+      self[*this_move] == '*' || closed_list.include?(this_move)
+    end
+  end
+    
+    
+  def initial_pathfind
+    # Adds all reachable cells from @start to the open list,
+    # and moves @start to the closed list.
+    # Cells have their pos and the adding cell (@start) saved to @parents.
+    reachable_cells.each do |cell| 
+      open_list << cell
+      parents[cell] = start
+    end
+    open_list.delete(location)
+    closed_list << location
+  end
+  
 end
 
-MazeSolver.new.display
+test = MazeSolver.new
+
+test.display
+test.initial_pathfind
+test.listing
