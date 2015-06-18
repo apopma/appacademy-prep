eb5dc0e4d162c5e7954db92a439c6c43ada0dce1
apopma <adam.popma@gmail.com>
2015-06-18 12:16:55 -0500

    Add tagging

--- w4/blogger/app/models/article.rb
+++ w4/blogger/app/models/article.rb
@@ -1,3 +1,23 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect { |tag| tag.strip.downcase }.uniq
+
+    new_or_found_tags = tag_names.collect do |tag_name|
+      Tag.find_or_create_by(name: tag_name)
+    end
+
+    self.tags = new_or_found_tags
+  end
+
+  def to_s
+    name
+  end
 end
