5f9db448e7de04e5fb0b2245ba2b1b0b8c462181
apopma <adam.popma@gmail.com>
2015-06-20 08:41:10 -0700

    paperclip pre Attachment model

--- w4/blogger/app/models/article.rb
+++ w4/blogger/app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ActiveRecord::Base
   has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :image, :content_type =>
     ["image/jpg", "image/jpeg", "image/png"]
+    # ask linda about Attachment model later
 
   def tag_list
     tags.collect {|tag| tag.name}.join(", ")
